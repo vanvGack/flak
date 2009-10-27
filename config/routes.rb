bbf3ccd0c38da2634246ab31e5e682576270c5c0
Nathan Sutton <nate@sevenwire.com>
2009-10-26 23:25:33 -0500

    Adding prune to allowed methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :users, :only => [:index, :create, :current], :collection => { :current => :get }
+  map.resources :users, :only => [:index, :create, :current, :prune], :collection => { :current => :get }
   map.resource  :session, :only => [:create, :destroy]
   map.resources :messages, :only => [:index, :create]
 end
