d8acce11494bb4cdcd8bbebfa248e5ec4df72a83
Brandon Arbini <brandon@sevenwire.com>
2009-10-26 20:00:57 -0500

    User list.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
+  before_filter :login_required, :only => :index
+
+  def index
+    @users = User.all
+    render :json => @users.to_json(:only => [:id, :email])
+  end
+
   def create
     logout_keeping_session!
     @user = User.new(params[:user])
