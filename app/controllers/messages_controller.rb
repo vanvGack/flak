41a2c64e12e8a14d30c5d264b97e225080025c65
Brandon Arbini <brandon@sevenwire.com>
2009-10-26 21:03:28 -0500

    Added from_id param to the messages index.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,9 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.find(:all) do
       paginate :page => params[:page], :per_page => params[:per_page]
+      if params[:from_id]
+        id > params[:from_id]
+      end
       order_by created_at.desc
     end
     render :json => @messages
