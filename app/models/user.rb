e8b199f6781cc0bde0d97f202ec9586d0e506cea
Nathan Sutton <nate@sevenwire.com>
2009-10-27 23:05:07 -0500

    Fixing tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
 
   has_many :messages
 
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -3,34 +3,41 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
 
   context UsersController do
-
     context "#create" do
-
-      should "return the id of the new account on success" do
-        response = post :create, :user => { :email => "asdf@asdf.com", :password => "asdfasdf", :password_confirmation => "asdfasdf" }
+      should "return the id of the new user on success" do
+        post :create, :user => Factory.attributes_for(:user)
         assert_response :success
-        assert_equal 1, JSON.parse(response.body)["user"]["id"]
+        assert JSON.parse(@response.body)["user"]["id"], "The id of the new user was not returned on success"
       end
 
       should "return errors on failure" do
-        response = post :create, :user => { :email => "asdf@asdf.com", :password => "asdfasdf", :password_confirmation => "asdf" }
+        post :create
         assert_response :success
-        assert_equal "Password doesn't match confirmation", JSON.parse(response.body).first
+        assert_same_elements JSON.parse(@response.body), User.create.errors.full_messages
       end
-
     end
 
-
     context "#index" do
-
       should "return the list of registered users" do
-        login_as(User.create(:email => "asdf@asdf.com", :password => "asdfasdf", :password_confirmation => "asdfasdf"))
+        login_as(Factory(:user))
         get :index
         assert_response :success
       end
-
     end
 
+    context "#current" do
+      should "return the list of non-stale users" do
+        user = Factory(:user)
+        login_as(user)
+        @stale_user = Factory(:stale_user)
+        @active_user = Factory(:active_user)
+        get :current
+        assert_response :success
+        body = JSON.parse(@response.body)
+        assert_equal user.id, body.first['user']['id']
+        assert_equal @active_user.id, body.last['user']['id']
+      end
+    end
   end
 
 end
