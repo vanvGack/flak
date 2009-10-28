b61558975bd7b6e0f7698af8b4b45be9f4c3a32e
Nathan Sutton <nate@sevenwire.com>
2009-10-27 20:39:49 -0500

    Fixing a weird rake bug

--- config/environment.rb
+++ config/environment.rb
@@ -16,7 +16,6 @@ Rails::Initializer.run do |config|
 
   config.gem 'forgery',      :source => 'http://gemcutter.org'
   config.gem 'factory_girl', :source => 'http://gemcutter.org'
-  config.gem 'shoulda',      :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
